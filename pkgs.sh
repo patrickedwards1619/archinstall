0614b2a0d453920bae7a894534eda98483366ead
soberhippie3 <91560218+soberhippie3@users.noreply.github.com>
2021-10-26 16:04:06 -0500

    Add files via upload

---
+++ pkgs.sh
@@ -0,0 +1,14 @@
+# essential packages
+base linux linux-firmware xorg-server xf86-video-amdgpu plasma-desktop sddm sddm-kcm
+    # if installing in a virtual machine, you can omit linux-firmware
+    # if installing in a container, you can omit linux and linux-firmware
+
+
+
+# neccessary packages
+bat findutils mlocate man-db man-pages kate breeze-gtk cpupower kde-gtk-config kscreen sudo nano
+
+
+
+# personal packages
+bitwarden bleachbit clamav clonezilla deluge-gtk digikam dolphin firefox
