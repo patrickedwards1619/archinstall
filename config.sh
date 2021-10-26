b1cc9a2b083110cad7d43cd9899aedd33ee86827
soberhippie3 <91560218+soberhippie3@users.noreply.github.com>
2021-10-26 16:51:45 -0500

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,68 @@
+# configure pacman
+sed -i 's #Color Color\nILoveCandy ; s #\[multilib\] \[multilib\] ; /\[multilib\]/{n;s #Include Include }' /etc/pacman.conf
+pacman -Sy
+
+
+# maybe install stuff from pkgs script here since we just now enabled the multilib repository
+#sleep 10
+
+# import user inputs from /root/confidentials
+read -r user uspw rtpw host tmzn < /root/confidentials
+
+#sleep 10
+
+# set the time and language
+ln -sf /usr/share/zoneinfo/$tmzn /etc/localtime
+hwclock --systohc
+sed -i 's #en_US.UTF-8 en_US.UTF-8 ' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+#sleep 10
+
+# set the hostname
+echo -e "$host" > /etc/hostname
+
+#sleep 10
+
+# configure the network          this may be a problem
+echo -e "127.0.0.1   localhost\n::1         localhost\n127.0.1.1   $host" >> /etc/hosts
+
+#sleep 10
+
+# set root password
+echo -e "$rtpw\n$rtpw" | passwd root
+
+#sleep 10
+
+# add a new user with password
+Useradd -m -g users -G wheel -s /bin/bash $user
+echo -e "$uspw\n$uspw" | passwd $user
+echo -e "%wheel ALL=(ALL) ALL\n" > /etc/sudoers.d
+
+
+# enable microcode updates
+pacman -S amd-ucode --noconfirm
+
+
+# install a bootloader           this may need to be changed to the way it is in doc
+pacman -S grub efibootmgr --noconfirm
+#sleep 10
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+#sleep 10
+grub-mkconfig -o /boot/grub/grub.cfg
+
+
+
+# enable systemd services
+pacman -S networkmanager --noconfirm
+systemctl enable NetworkManager sddm
+
+
+# exit the chroot environment
+echo -e "\nExiting the chroot environment in 10 seconds\n"
+sleep 10
+
+
+
+
---
+++ install.sh
@@ -0,0 +1,142 @@
+# get user input
+echo -e "\nEnter username to be created:\n"
+read user
+
+
+echo -e "\nEnter new password for $user:\n"
+read uspw
+
+
+echo -e "\nEnter new password for root:\n"
+read rtpw
+
+
+echo -e "\nEnter new hostname (device name):\n"
+read host
+
+
+echo -e "\nEnter desired timezone in format of Country/Region (the list of all timezones can be found by first switching to different virtual console using Alt+arrow shorcut and issuing command \"ls /usr/share/zoneinfo/*/* | less\"):\n"
+read tmzn
+#maybe excluse switching to a different console
+
+
+echo -e "\nEnter the name of the hard drive you would like to use in format of /dev/<drive_name> (Drives can be listed by first switching to different virtual console using Alt+arrow shorcut and issuing command \"fdisk -l/*/* | less\"):\n"
+read drve
+
+
+echo -e "\nEnter the percentage of the drive you want to reserve for efi partition (don't include %):\n"
+read drsz
+
+
+# save these inputs in a file from which the respective fields will be sourced later
+echo -e "$user $uspw $rtpw $host $tmzn $drve $drsz" > ./confidentials
+
+
+echo -e "\nDone.\n\n"
+
+#sleep 10
+
+
+
+
+
+# verify that the system is booted in UEFI mode with…
+ls /sys/firmware/efi/efivars
+
+#sleep 10
+
+# verify that the internet is working
+ping -c 5 archlinux.org
+
+#sleep 10
+
+
+# wipe drive
+wipefs --all "$drve"
+
+# format Drive
+parted -s -a optimal "$drve" mklabel gpt
+parted -s -a optimal "$drve" mkpart "boot" 0% "$drsz"%
+parted -s -a optimal "$drve" set 1 esp on
+parted -s -a optimal "$drve" mkpart "root" "$drsz"% 100%
+yes | mkfs.fat -F32 "$drve"1
+yes | mkfs.ext4 "$drve"2
+
+#sleep 10
+
+
+# mount partitions           this may need to be changed to the way it is in doc
+mount "$drve"2 /mnt
+mkdir /mnt/boot
+mount "$drve"1 /mnt/boot
+
+#sleep 10
+
+# update the system clock
+timedatectl set-ntp true
+
+#sleep 10
+
+
+# configure pacman  MAYBE MOVE THIS SOMEWHERE ELSE
+#sed -i 's #Color Color ; s #\[multilib\] \[multilib\] ; /\[multilib\]/{n;s #Include Include }' /etc/pacman.conf
+#sleep 10
+#echo -e "ILoveCandy" > /etc/pacman.conf
+
+#sleep 10
+
+# update pacman repositories
+pacman -Sy
+
+#sleep 10
+
+#might need to install reflector
+# update the mirror list
+reflector -c US --latest 20 --sort rate --download-timeout 120 --connection-timeout 120 --save /etc/pacman.d/mirrorlist
+
+
+#sleep 60
+
+
+# install packages from the "pkgs" file
+pacstrap /mnt $(cat pkgs.sh | sed 's #.*$  g' | tr '\n' ' ')
+
+#sleep 10
+#change out of the archinstall directory (might want to delete this line)
+#cd
+
+# generate an fstab file
+genfstab -U /mnt >> /mnt/etc/fstab
+
+
+# prepeare to change root into the new system and run config script
+
+# copy the confidentials file to destination system's root partition so that config script can access the file from inside of chroot
+cp ./confidentials /mnt/root/
+
+# copy the config script to destination system's root partition
+cp ./config.sh /mnt/root/
+
+# change file permission of config script to make it executable
+chmod a+x /mnt/root/config.sh
+
+
+# change root into the new system and run config script
+echo -e "\nChrooting into the new environment in 10 seconds\n"
+sleep 10
+arch-chroot /mnt /root/config.sh #might need to remove /root/config here to get config script to run
+
+
+# remove files that are now unnecessary
+rm /mnt/root/{confidentials,config.sh}
+
+
+# unmount all partitions
+umount -R /mnt
+
+# reboot the system
+echo -e "\nInstallation Complete.\n\nSystem will reboot in 10 seconds..."
+
+sleep 10
+
+reboot
